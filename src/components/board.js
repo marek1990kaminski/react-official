595e6fa98a99d7c326ce6019ca46dac55a36ba01
Marek K <marekkaminski26@gmail.com>
2017-11-18 19:19:15 +0100

    teraz podzielilem pliki tak, ze na jeden wypada jeden component

---
+++ src/components/board.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Square from './square';
+
+class Board extends React.Component {
+
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+            />
+        )
+    }
+
+//this method is completelly additional, we may do without it
+    manySquares(start, end) {
+        const squares = [];
+
+        for (let i = start; i <= end; i++) {
+            squares.push(this.renderSquare(i));
+        }
+        return squares;
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.manySquares(0, 2)}
+                    {/*{this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}*/}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Board;
--- src/index.js
+++ src/index.js
@@ -1,59 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Board from './components/board';
 import './index.css';
 
-function Square(props) {
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
-}
 
-class Board extends React.Component {
-
-    renderSquare(i) {
-        return (
-            <Square
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
-            />
-        )
-    }
-
-//this method is completelly additional, we may do without it
-    manySquares(start, end) {
-        const squares = [];
-
-        for (let i = start; i <= end; i++) {
-            squares.push(this.renderSquare(i));
-        }
-        return squares;
-    };
-
-    render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.manySquares(0, 2)}
-                    {/*{this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}*/}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
 
 class Game extends React.Component {
     constructor(props) {
